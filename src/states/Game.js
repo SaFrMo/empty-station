5d9c16923cec1c995a0d057da062a1298275847e
Sander Moolin <sandermoolin@gmail.com>
2018-04-03 21:38:33 -0400

    Initial

---
+++ src/states/Game.js
@@ -0,0 +1,21 @@
+/* globals __DEV__ */
+import Phaser from 'phaser'
+
+export default class extends Phaser.State {
+  init () {}
+  preload () {}
+
+  create () {
+
+      // game entry point!
+
+  }
+
+  render () {
+    if (__DEV__) {
+
+        // dev mode entry point!
+
+    }
+  }
+}
